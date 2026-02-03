b74d677db95a9a89f1b4e8f068b5c7b10dc50d7c
Chaitanya Davuluri <150474331+MrChaitu111@users.noreply.github.com>
2026-02-04 02:08:43 +0530

    Add comments to routes and ToastContainer in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import Signup from "./Signup";
 import Login from "./Login";
 import Home from "./Home";
 
-import { ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify"; //toast for using alert pop up message either success or error
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <ToastContainer position="top-center" />
+      <ToastContainer position="top-center" /> {/* toast message will show the top center */}
 
       <Routes>
-        <Route path="/" element={<Navigate to="/signup" />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Navigate to="/signup" />} /> {/* Navbar  Router (app/) */}
+        <Route path="/signup" element={<Signup />} />{/* SignUp Router (app/signup) */}
+        <Route path="/login" element={<Login />} />{/* Login Router (app/login) */}
+        <Route path="/home" element={<Home />} />{/* Home Router (app/home) */}
       </Routes>
     </BrowserRouter>
   );
